413de9fb6d9774301d9d3424cf6839598c706f3e
Gregor Haas <gregorhaas1997@gmail.com>
2019-12-07 14:22:10 -0500

    Changes

--- main.c
+++ main.c
@@ -9,15 +9,5 @@ int main()
         printf("Failed to exploit device\n");
         return status;
     }
-    else
-    {
-        unsigned char aes_in[16] = {0xDE, 0xAD, 0xBE, 0xEF,
-                                    0xDE, 0xAD, 0xBE, 0xEF,
-                                    0xDE, 0xAD, 0xBE, 0xEF,
-                                    0xDE, 0xAD, 0xBE, 0xEF};
-        unsigned char aes_out[16];
 
-        aes(aes_in, aes_out, AES_ENCRYPT, AES_UID_KEY);
-        printf("%s\n", aes_out);
-    }
 }
